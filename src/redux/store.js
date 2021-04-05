214c32d4a56842c35d71bd0bedb4e635b05c07da
tcannetti <tcannetti@gmail.com>
2021-04-05 13:18:03 -0500

    inital redux commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,6 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import reducers from './reducers'
+import thunk from 'redux-thunk'
 import state from './state'
 
-export default createStore(reducers, state)
+export default createStore(reducers, state, applyMiddleware(thunk))
